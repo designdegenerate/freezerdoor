85be48828a6ed6bc42eb7da094eb65cbc305179c
lspangenberg <laurens.am.spangenberg@gmail.com>
2022-07-15 13:29:27 +0200

    build container for canvas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <nav>
+        <h1>The Cool Wall</h1>
+      </nav>
+      <div id="canvas-wrapper">
+        <canvas></canvas>
+        <div id="grid">
+          <div id="col-one">
+            <h2>Seriously Uncool</h2>
+          </div>
+          <div id="col-two">
+            <h2>Uncool</h2>
+          </div>
+          <div id="col-three">
+            <h2>Cool</h2>
+          </div>
+          <div id="col-four">
+            <h2>Subzero</h2>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
