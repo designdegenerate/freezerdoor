2d1e16c6489ff6d66088694395aaa21add6abe12
lspangenberg <laurens.am.spangenberg@gmail.com>
2022-07-15 19:59:42 +0200

    integrate react hook forms, styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { Layer, Stage, Text, Label, Group, Image, Tag } from "react-konva";
 import useImage from "use-image";
 import "./App.css";
 
 function App() {
+  const { register, handleSubmit } = useForm();
   const [getCards, setCards] = useState([
     {
       id: 1,
@@ -21,26 +23,35 @@ function App() {
     },
   ]);
 
-  const ImgCard = ({ url }) => {
-    const [image] = useImage(url);
+  const calculateIdealSize = (url) => {
+    let image = new Image();
+    image.src = url;
 
-    // Put them 0 so they don't render,
-    // and also not crash
     let imgWidth = 0;
     let imgHeight = 0;
 
-    if (image) {
-      if (image.width > 200) {
-        const scalingFactor = image.width / 200;
-        imgWidth = 200;
-        imgHeight = image.height / scalingFactor;
-      } else {
-        imgWidth = image.width;
-        imgHeight = image.height;
-      }
+    if (image.width > 200) {
+      const scalingFactor = image.width / 200;
+      imgWidth = 200;
+      imgHeight = image.height / scalingFactor;
+    } else {
+      imgWidth = image.width;
+      imgHeight = image.height;
     }
 
-    return <Image width={imgWidth} height={imgHeight} image={image} />;
+    //garbage collect
+    image = null;
+
+    return {
+      width: imgWidth,
+      height: imgHeight,
+    };
+  };
+
+  const ImgCard = ({ url, width, height }) => {
+    const [image] = useImage(url);
+
+    return <Image width={width} height={height} image={image} />;
   };
 
   return (
@@ -48,18 +59,47 @@ function App() {
       <nav>
         <h1>The Cool Wall</h1>
       </nav>
+      <details>
+        <summary>+ Create New Entry</summary>
+        <form
+          onSubmit={handleSubmit((data) => {
+            console.log(data);
+
+            const idealSize = calculateIdealSize(data.url);
+
+            const newCard = {
+              id: getCards.length + 1,
+              url: data.url,
+              width: idealSize.width,
+              height: idealSize.height,
+              text: data.text,
+            };
+
+            console.log(newCard);
+
+            setCards([...getCards, newCard]);
+          })}
+        >
+          <label htmlFor="title">Title</label>
+          <input {...register("title")} id="title" type="text"></input>
+          <label htmlFor="url">Image URL</label>
+          <input {...register("url")} id="url" type="url"></input>
+          <button type="submit">Create</button>
+        </form>
+      </details>
       <div id="canvas-wrapper">
-        <Stage width={2000} height={2000}>
+        <Stage width={3000} height={1000}>
           <Layer>
             {getCards.map((card) => {
               return (
-                <Group draggable={true}>
+                <Group key={card.id} draggable={true}>
                   <ImgCard
-                    key={card.id}
                     url={card.url}
+                    width={card.width}
+                    height={card.height}
                   />
                   <Label offsetY={20}>
-                    <Tag fill={"white"} lineJoin={"round"} />
+                    <Tag fill={"white"} />
                     <Text
                       fontStyle={"bold"}
                       padding={4}
